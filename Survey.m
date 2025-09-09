962caef8d3efffc1657d7e9d12cc5482d2c115c6
Justin Frandsen <jfran4047@gmail.com>
2025-09-08 21:04:57 -0500

    Remove survey

--- Survey.m
+++
@@ -1,7 +0,0 @@
-% Consent Form
-print("!!!! INCORRECT VERSION OF CONSENT FORM !!!! UPDATE BEFORE RUNNING !!!!")
-surveyLink1 = 'https://tamu.qualtrics.com/jfe/form/SV_3rAqu5uGsnGqsYu';
-% Demographics - Global_Salience
-surveyLink2 = 'https://tamu.qualtrics.com/jfe/form/SV_3ObKa8a8euAs5ds';
-system(['start chrome "' surveyLink1 '"']);
-system(['start chrome "' surveyLink2 '"']);
